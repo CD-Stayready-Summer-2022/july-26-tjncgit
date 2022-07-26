032bde889fc2f7eca201e82b58e71db19fa22047
Tamajong Nchukwi <tjnc@Tamajongs-MacBook-Pro-2.local>
2022-07-26 09:23:24 -0400

    tests pass

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,22 @@
 package com.codedifferently;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
-        return null;
+        String[] stringArr = input.split("");
+        Map<String, Integer> countMap = new TreeMap<>();
+        for(String letter: stringArr){
+            if(countMap.containsKey(letter)){
+                Integer val = countMap.get(letter);
+                countMap.put(letter, val + 1);
+            } else {
+                countMap.put(letter, 1);
+            }
+        }
+        String out = countMap.toString();
+        out = out.substring(1, out.length() - 1).replace("=", ":").replace(",", "");
+        return out;
     }
 }
